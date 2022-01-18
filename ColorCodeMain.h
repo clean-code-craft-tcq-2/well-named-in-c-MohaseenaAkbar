d8b8354976168196ce0172a9c7592b5b3ecf344f
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-01-18 21:03:17 +0530

    Update ColorCodeMain.h

--- ColorCodeMain.h
+++ ColorCodeMain.h
@@ -10,8 +10,6 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
