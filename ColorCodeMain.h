2df8bc98f5867d485c5d45341c24effb034cc3e1
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-01-18 21:10:52 +0530

    Update ColorCodeMain.h

--- ColorCodeMain.h
+++ ColorCodeMain.h
@@ -7,16 +7,6 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 extern int numberOfMinorColors;
 
 typedef struct {
